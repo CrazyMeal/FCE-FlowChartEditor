4265a4815fc9ab8348589e74a2b75fcdcdd3667c
KVN <kvndesign.mail@gmail.com>
2014-11-21 14:01:07 +0100

    Making different panels

--- js/alubar-app.js
+++ js/alubar-app.js
@@ -1,5 +1,13 @@
 var app = angular.module('alubar-app', []);
 
 app.controller('MainCtrl', function ($scope) {
-    
+    this.tab = 1;
+
+    this.selectTab = function(setTab) {
+        this.tab = setTab;
+    };
+
+    this.isSelected = function(checkTab) {
+        return this.tab === checkTab;
+    };
 });
