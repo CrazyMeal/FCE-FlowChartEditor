6e795bb4ca8a8ce14f1ed9eaf0a8e30a455d4393
KVN <kvndesign.mail@gmail.com>
2014-11-23 17:06:25 +0100

    States drawing
    Can draw states but still problem when drawing connections

--- js/alubar/controllers/new-doc-ctrl.js
+++ js/alubar/controllers/new-doc-ctrl.js
@@ -1,13 +1,102 @@
 var app = angular.module('alubar-app');
 app.controller('NewDocCtrl', function($scope){
     $scope.init = function() {
+        /*
         jsPlumb.bind("ready", function() {
             console.log("Set up jsPlumb listeners (should be only done once)");
+            
             jsPlumb.bind("connection", function (info) {
                 $scope.$apply(function () {
                     console.log("Possibility to push connection into array");
                 });
+            });  
+            
+        });
+        */
+    };
+});
+jsPlumb.ready(function() {
+
+  jsPlumb.setContainer($('#plumbing-zone'));
+  var i = 0;
+
+  $('#plumbing-zone').dblclick(function(e) {
+    var newState = $('<div>').attr('id', 'state' + i).addClass('item');
+    
+    var title = $('<div>').addClass('title').text('State ' + i);
+    var connect = $('<div>').addClass('connect');
+    
+    newState.css({
+      'top': e.pageY- $('#plumbing-zone').offset().top,
+      'left': e.pageX - $('#plumbing-zone').offset().left
+    });
+    
+    newState.append(title);
+    newState.append(connect);
+    
+     jsPlumb.draggable(newState, {
+      containment: 'parent'
+    });
+    newState.dblclick(function(e) {
+      jsPlumb.detachAllConnections($(this));
+      $(this).remove();
+      e.stopPropagation();
+    });
+    $('#plumbing-zone').append(newState);
+
+    jsPlumb.makeTarget(newState, {
+      anchor: 'Continuous'
+    });
+    
+    jsPlumb.makeSource(connect, {
+      parent: newState,
+      anchor: 'Continuous'
+    });
+    
+    i++;    
+  });  
+});
+/*
+jsPlumb.ready(function() {
+            jsPlumb.setContainer($('#plumbing-zone'));
+
+            var i = 0;
+            $('#plumbing-zone').dblclick(function(e) {
+                var newState = $('<div>').attr('id', 'state' + i).addClass('sampleElement');
+            
+                var title = $('<div>').addClass('title').text('State ' + i);
+                var connect = $('<div>').addClass('connect');
+            
+                newState.css({
+                    'top': e.pageY,
+                    'left': e.pageX - $('#plumbing-zone').offset().left
+                });
+                newState.append(title);
+                newState.append(connect);
+                
+                jsPlumb.draggable(newState, {
+                    containment: 'parent'
+                });
+                 
+                newState.dblclick(function(e) {
+                    jsPlumb.detachAllConnections($(this));
+                    $(this).remove();
+                    e.stopPropagation();
+                });
+                
+                $('#plumbing-zone').append(newState);
+ 
+                jsPlumb.makeTarget(newState, {
+                    anchor: 'Continuous'
+                });
+                
+                jsPlumb.makeSource(connect, {
+                    parent: newState,
+                    anchor: 'Continuous'
+                });
+                
+                i++; 
             });
         });
     };
-});
+*/
