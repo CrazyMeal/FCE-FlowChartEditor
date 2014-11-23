3d89c27625e61284f7295f388e742efd60cc8911
KVN <kvndesign.mail@gmail.com>
2014-11-23 18:28:41 +0100

    Corrected bug
    Now connections are being made properly

--- js/alubar/controllers/new-doc-ctrl.js
+++ js/alubar/controllers/new-doc-ctrl.js
@@ -42,16 +42,15 @@ jsPlumb.ready(function() {
       $(this).remove();
       e.stopPropagation();
     });
-    $('#plumbing-zone').append(newState);
 
-    jsPlumb.makeTarget(newState, {
-      anchor: 'Continuous'
+    jsPlumb.makeTarget(connect, {
+        anchor: 'Continuous'
     });
     
     jsPlumb.makeSource(connect, {
-      parent: newState,
       anchor: 'Continuous'
     });
+    $('#plumbing-zone').append(newState);
     
     i++;    
   });  
