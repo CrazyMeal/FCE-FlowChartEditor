b99d410ebd7b66e35144023e94bf2d19aab9ce3c
KVN <kvndesign.mail@gmail.com>
2014-11-25 21:31:58 +0100

    Bug

--- js/alubar/controllers/new-doc-ctrl.js
+++ js/alubar/controllers/new-doc-ctrl.js
@@ -1,66 +1,57 @@
 var app = angular.module('alubar-app');
 app.controller('NewDocCtrl', function($scope){
+    $scope.documentName = "Nouveau Document";
+    $scope.states = [];
+
     $scope.init = function() {
-        /*
-        jsPlumb.bind("ready", function() {
-            console.log("Set up jsPlumb listeners (should be only done once)");
+        jsPlumb.ready(function() {
+            jsPlumb.setContainer($('#plumbing-zone'));
+
+            var i = 0;
+            /*
+            jsPlumb.bind('connection', function(info) {
+              alert('New connection!\nFrom: ' + info.sourceId + '\nTo: ' + info.targetId);
+            });
+            */
+            $('#plumbing-zone').dblclick(function(e) {
+                var newState = $('<div>').attr('id', 'state' + i).addClass('state');
             
-            jsPlumb.bind("connection", function (info) {
-                $scope.$apply(function () {
-                    console.log("Possibility to push connection into array");
+                var title = $('<div>').addClass('title').text('State ' + i);
+                var connect = $('<div>').addClass('connect');
+                
+                newState.css({
+                    'top': e.pageY- $('#plumbing-zone').offset().top,
+                    'left': e.pageX - $('#plumbing-zone').offset().left
                 });
+                
+                newState.append(title);
+                newState.append(connect);
+                
+                jsPlumb.draggable(newState, {
+                    containment: 'parent'
+                });
+                newState.dblclick(function(e) {
+                    jsPlumb.detachAllConnections($(this));
+                    $(this).remove();
+                    e.stopPropagation();
+                });
+
+                jsPlumb.makeTarget(connect, {
+                    anchor: 'Continuous'
+                });
+                
+                jsPlumb.makeSource(connect, {
+                    anchor: 'Continuous'
+                });
+                $('#plumbing-zone').append(newState);
+                $scope.states.push(newState);
+                console.log($scope.states);
+                i++;    
             });  
-            
         });
-        */
     };
 });
-jsPlumb.ready(function() {
 
-  jsPlumb.setContainer($('#plumbing-zone'));
-  var i = 0;
-
-  $('#plumbing-zone').dblclick(function(e) {
-    var newState = $('<div>').attr('id', 'state' + i).addClass('item');
-    
-    var title = $('<div>').addClass('title').text('State ' + i);
-    var connect = $('<div>').addClass('connect');
-    
-    newState.css({
-      'top': e.pageY- $('#plumbing-zone').offset().top,
-      'left': e.pageX - $('#plumbing-zone').offset().left
-    });
-    
-    newState.append(title);
-    newState.append(connect);
-    
-     jsPlumb.draggable(newState, {
-      containment: 'parent'
-    });
-    newState.dblclick(function(e) {
-      jsPlumb.detachAllConnections($(this));
-      jsPlumb.detachAllConnections($(this).children()[1]);
-      
-
-      $(this).remove();
-      console.log($(this).children()[1]);
-      e.stopPropagation();
-    });
-    $('#plumbing-zone').append(newState);
-
-    jsPlumb.makeTarget(connect, {
-        parent: connect,
-        anchor: 'Continuous'
-    });
-    
-    jsPlumb.makeSource(connect, {
-      parent: connect,
-      anchor: 'Continuous'
-    });
-    
-    i++;    
-  });  
-});
 /*
 jsPlumb.ready(function() {
             jsPlumb.setContainer($('#plumbing-zone'));
