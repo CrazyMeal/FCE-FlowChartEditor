2a1542fec49d6387fb96b720c0b2319a7ac964b4
KVN <kvndesign.mail@gmail.com>
2014-11-28 21:50:34 +0100

    Beginning "Angularisation" for jsPlumb part

--- js/alubar/controllers/new-doc-ctrl.js
+++ js/alubar/controllers/new-doc-ctrl.js
@@ -3,11 +3,37 @@ app.controller('NewDocCtrl', function($scope){
     $scope.documentName = "Nouveau Document";
     $scope.states = [];
 
+    setStateCss = function(state, e){
+        state.container.css({
+            'top': e.pageY- $('#plumbing-zone').offset().top,
+            'left': e.pageX - $('#plumbing-zone').offset().left
+        });
+    };
+
+    $scope.createNewState = function(){
+        var mainContainer = $('<div>').attr('id', 'state' + $scope.states.length).addClass('state');
+            
+        var titleDiv = $('<div>').addClass('title').text('State ' + $scope.states.length);
+        var connectInDiv = $('<div>').addClass('connectIn');
+        var connectOutDiv = $('<div>').addClass('connectOut');
+        mainContainer.append(titleDiv);
+        mainContainer.append(connectInDiv);
+        mainContainer.append(connectOutDiv);
+
+        var state = {
+            container: mainContainer,
+            title: titleDiv,
+            input: connectInDiv,
+            outPut: connectOutDiv
+        };
+
+        $scope.states.push(state);
+        return state;
+    };
+
     $scope.init = function() {
         jsPlumb.ready(function() {
             jsPlumb.setContainer($('#plumbing-zone'));
-
-            var i = 0;
             /*
             jsPlumb.bind('connection', function(info) {
               alert('New connection!\nFrom: ' + info.sourceId + '\nTo: ' + info.targetId);
@@ -18,47 +44,41 @@ app.controller('NewDocCtrl', function($scope){
             });
 
             $('#plumbing-zone').dblclick(function(e) {
-                var newState = $('<div>').attr('id', 'state' + i).addClass('state');
-            
-                var title = $('<div>').addClass('title').text('State ' + i);
-                var connect = $('<div>').addClass('connect');
+                var newState = $scope.createNewState();
                 
-                newState.css({
-                    'top': e.pageY- $('#plumbing-zone').offset().top,
-                    'left': e.pageX - $('#plumbing-zone').offset().left
+                jsPlumb.draggable(newState.container, {
+                    containment: $('#plumbing-zone')
                 });
                 
-                newState.append(title);
-                newState.append(connect);
-                
-                jsPlumb.draggable(newState, {
-                    containment: 'parent'
-                });
-                newState.dblclick(function(e) {
+                setStateCss(newState, e);
+
+                newState.container.dblclick(function(e) {
                     jsPlumb.detachAllConnections($(this));
-                    jsPlumb.detachAllConnections($(this).children()[1]);
+
+                    angular.forEach($(this).children(), function(child){
+                        jsPlumb.detachAllConnections(child);
+                        child.remove();
+                    });
+
                     $(this).remove();
                     e.stopPropagation();
                 });
 
-                jsPlumb.makeTarget(connect, {
-                    parent: connect,
-                    connector:[ "Flowchart" ],
-                    anchor: 'Continuous',
+                jsPlumb.makeTarget(newState.input, {
+                    //parent: newState.container,
+                    connector:[ "Flowchart" ,{ stub:[40, 60], cornerRadius:5, alwaysRespectStubs:true }],
+                    anchor: ['Continuous',{ faces:[ "left" ] }]
                 });
                 
-                jsPlumb.makeSource(connect, {
-                    parent: connect,
-                    connector:[ "Flowchart" ],
-                    anchor: 'Continuous',
+                jsPlumb.makeSource(newState.outPut, {
+                    //parent: newState.container,
+                    connector:[ "Flowchart" ,{ stub:[40, 60], cornerRadius:5, alwaysRespectStubs:true }],
+                    anchor: ['Continuous',{ faces:["right"] }],
                     connectorOverlays:[ 
                         [ "Arrow", { width:20, length:30, location:1, id:"arrow" } ]
                     ]
                 });
-                $('#plumbing-zone').append(newState);
-                $scope.states.push(newState);
-                console.log($scope.states);
-                i++;    
+                $('#plumbing-zone').append(newState.container);  
             });  
         });
     };
