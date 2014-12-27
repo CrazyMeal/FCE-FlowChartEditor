dde7e175304567e9f09d959b207bb33c7790508a
KVN <kvndesign.mail@gmail.com>
2014-12-27 15:42:56 +0100

    Added some verifications

--- js/alubar/controllers/state-edition-ctrl.js
+++ js/alubar/controllers/state-edition-ctrl.js
@@ -1,22 +1,42 @@
 var app = angular.module('alubar-app');
 
 app.controller('StateEditionCtrl', function($scope) {
-	$scope.dropped = function(dragEl, dropEl, posX, posY) {
- 		var dragEl = document.getElementById(dragEl);
-  		var dropEl = document.getElementById(dropEl);
-  		var drag = angular.element(dragEl);
-      	var drop = angular.element(dropEl);
+  assignClass = function(droppedElement, newComponent){
+    if(droppedElement.hasClass("pdf-component")){
+      newComponent.addClass("pdf-component");
+      return true;
+    }
+    if(droppedElement.hasClass("video-component")){
+      newComponent.addClass("video-component");
+      return true;
+    }
+    if(droppedElement.hasClass("tchat-component")){
+      newComponent.addClass("tchat-component");
+      return true;
+    }
+  };
 
+  $scope.dropped = function(dragEl, dropEl, posX, posY) {
+ 	  var dragEl = document.getElementById(dragEl);
+  	var dropEl = document.getElementById(dropEl);
+  	var drag = angular.element(dragEl);
+    var drop = angular.element(dropEl);
 
-    	console.log("The element " + drag.attr('id') + " has been dropped on " + drop.attr("id") + "!");
-    	console.log("X: "+ posX + " Y: " + posY);
+    if(drag.hasClass("component")){
+      console.log("The element " + drag.attr('id') + " has been dropped on " + drop.attr("id") + "!");
+      console.log("X: "+ posX + " Y: " + posY);
+      
+      var component = $('<div>').addClass('dropped-component');
+      component.css({
+        'top': posY - $('#working-zone').offset().top - 25,
+        'left': posX - $('#working-zone').offset().left - 25
+      });
 
-    	var component = $('<div>').addClass('dropped-component');
-    	component.css({
-            'top': posY - $('#working-zone').offset().top - 25,
-            'left': posX - $('#working-zone').offset().left - 25
-        });
+      assignClass(drag, component);
+      
+      drop.append(component);
+    }
+    
 
-        drop.append(component);
     };
 });
