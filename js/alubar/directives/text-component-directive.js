555881873b466bd89bfc527d18b1a2127dfab301
KVN <kvndesign.mail@gmail.com>
2015-02-10 22:03:32 +0100

    Can now edit text component by double click

--- js/alubar/directives/text-component-directive.js
+++ js/alubar/directives/text-component-directive.js
@@ -7,9 +7,14 @@ app.directive('textcomponent', function($compile){
         scope: {
             text: '='
         },
-        template: '<div class="dropped-component">{{text}}</div>',
+        template: '<div class="dropped-component text-zone-dropped"><p ng-show="!editingText">{{text}}</p><textarea ng-show="editingText" ng-model="text"></textarea></div>',
         link: function(scope, element, attrs){
-            
+            scope.editingText = false;
+            $(element).dblclick(function(event){
+                console.log("double click on text component");
+                scope.editingText = !scope.editingText;
+                scope.$apply();
+            });
         }
     };
 });
